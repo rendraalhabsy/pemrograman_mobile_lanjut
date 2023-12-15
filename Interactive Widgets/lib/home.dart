159a320c7835c57e4f4e7d6e8138cd24ea4c2b8a
risayyaa29 <153906986+rendraalhabsy@users.noreply.github.com>
2023-12-15 17:43:39 +0700

    LKP - Interactive Widgets

---
+++ Interactive Widgets/lib/home.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: unused_import, unused_field, library_private_types_in_public_api, unused_element, deprecated_member_use, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'models/explore_recipe.dart';
+import 'components/components.dart';
+import 'screens/explore_screen.dart';
+import 'screens/recipes_screen.dart';
+import 'screens/grocery_screen.dart';
+import 'package:provider/provider.dart';
+import 'models/models.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static List<Widget> pages = <Widget>[
+    ExploreScreen(),
+    RecipesScreen(),
+    const GroceryScreen()
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TabManager>(
+      builder: (context, tabManager, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Fooderlich',
+                style: Theme.of(context).textTheme.headline6),
+          ),
+          // 2
+          body: pages[tabManager.selectedTab],
+          bottomNavigationBar: BottomNavigationBar(
+            selectedItemColor:
+                Theme.of(context).textSelectionTheme.selectionColor,
+            // 3
+            currentIndex: tabManager.selectedTab,
+            onTap: (index) {
+              // 4
+              tabManager.goToTab(index);
+            },
+            items: <BottomNavigationBarItem>[
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.explore),
+                label: 'Explore',
+              ),
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.bookmark),
+                label: 'Recipes',
+              ),
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.menu),
+                label: 'To Buy',
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
